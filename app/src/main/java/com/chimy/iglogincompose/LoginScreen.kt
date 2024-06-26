498063315231ecebeaea36d0f780587dc2ad992e
michimurri <joselo.okumura@gmail.com>
2024-06-26 18:37:48 -0300

    EmailPassword ET

--- app/src/main/java/com/chimy/iglogincompose/LoginScreen.kt
+++ app/src/main/java/com/chimy/iglogincompose/LoginScreen.kt
@@ -1,16 +1,27 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.chimy.iglogincompose
 
 import android.app.Activity
+import android.provider.ContactsContract.CommonDataKinds.Email
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.modifier.modifierLocalConsumer
@@ -33,9 +44,24 @@ fun LoginScreen() {
 
 @Composable
 fun Body(modifier: Modifier) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
     Column(modifier = modifier) {
         ImageLogo()
+        Spacer(modifier = Modifier.size(16.dp))
+        Email(email) { email = it }
+        Spacer(modifier = Modifier.size(4.dp))
+        Password(password) {password = it }
     }
+}
+
+@Composable
+fun Password (password: String, onTextChanged: (String) -> Unit) {
+    TextField(value = password,  onValueChange = {onTextChanged(it)} )
+}
+@Composable
+fun Email(email: String, onTextChanged: (String) -> Unit) {
+    TextField(value = email,  onValueChange = {onTextChanged(it)} )
 }
 
 @Composable
